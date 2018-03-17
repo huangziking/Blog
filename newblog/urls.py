c12e75954635a57ff65afda4b5ec44cbbfecad1c
huangziking <1475495884@qq.com>
2018-03-18 01:02:08 +0800

    detail
    new detail

--- newblog/urls.py
+++ newblog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'newblog'
 urlpatterns=[
-    url(r'^$',views.index,name='index')
+    url(r'^$',views.index,name='index'),
+    url('post/(?<pk>[0-9]+)/$',views.detail,name='detail')
 ]
--- newblog/views.py
+++ newblog/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Post
 def index(request):
     return render(request,'newblog/index.html',context={
         'title':'我的首页',
         'welcome':'欢迎访问的的首页'
     })
+def detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request,'newblog/detail.html',context={'post':post})

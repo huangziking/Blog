5c2a7bbb7caee6211ae358709e04a407bcbd060e
huangziking <1475495884@qq.com>
2018-03-19 17:53:17 +0800

    view 模板
    视图模板

--- newblog/urls.py
+++ newblog/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'newblog'
 urlpatterns=[
     url(r'^$',views.index,name='index'),
-    url('post/(?<pk>[0-9]+)/$',views.detail,name='detail')
+    url('^post/(?P<pk>[0-9]+)/$',views.detail,name='detail')
 ]
--- newblog/views.py
+++ newblog/views.py
@@ -4,11 +4,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render,get_object_or_404
 from .models import Post
-def index(request):
+'''def index(request):
     return render(request,'newblog/index.html',context={
         'title':'我的首页',
         'welcome':'欢迎访问的的首页'
     })
+'''
+def index(request):
+    post_list=Post.objects.all().order_by('-creat_time')
+    print(post_list[0].get_absolute_url)
+    return render(request,'newblog/index.html',context={'post_list':post_list})
 def detail(request,pk):
     post = get_object_or_404(Post,pk=pk)
     return render(request,'newblog/detail.html',context={'post':post})
